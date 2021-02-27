4ba9efd11e2332930375e55f9cc91e5c3ee2dfc4
U-WangE <52265130+U-WangE@users.noreply.github.com>
2021-02-28 00:26:04 +0900

    No.11021 (Baekjoon)

---
+++ 11021.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+
+int main() {
+	int n, a, b, i;
+
+	scanf("%d", &n);
+
+	for (i = 1;i <= n;i++) {
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", i, a + b);
+	}
+}
