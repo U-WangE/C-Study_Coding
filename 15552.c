13fd839334182b28602306b1a24c6a1e3ca2c400
U-WangE <52265130+U-WangE@users.noreply.github.com>
2021-02-28 00:36:02 +0900

    No.15552 (Baekjoon)

---
+++ 15552.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+
+int main() {
+	int t, a, b, i;
+	int *arr;
+	scanf("%d", &t);
+	arr = (int*)malloc(sizeof(int)*t);
+
+	for (i = 0; i < t; i++) {
+		scanf("%d %d", &a, &b);
+		arr[i] = a + b;
+	}
+
+	for (i = 0; i < t; i++) {
+		printf("%d\n", arr[i]);
+	}
+}
